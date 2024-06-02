b72e4d5974a5634a37092f13d2f1a3620e80372e
shahidullah <shohidkhan4361@gmail.com>
2024-06-02 11:57:03 +0600

    e-commerce-tailwind-top-footer done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./assets/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
